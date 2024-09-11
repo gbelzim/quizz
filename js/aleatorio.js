07040299e0f44a1ace1f3d56b4aaabbaeea26cb9
Gabriel H <gabriel.mendes.oliveira01@escola.pr.gov.br>
2024-09-11 16:03:01 -0300

    cagada man

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Matue", "indio flecha torta pussy da silva muxa", "Leandro mussum", "Carl Johnson", "Seu Madroga", "Amostradinho (madito)", "cristiano da silva sauro pinto alcantra caicique classico jorge do pneu broxa azulzinho vencido da rosa vulgo tadala junior"];
+const nomes = ["Matue", "indio flecha torta pussy da silva muxa", "Leandro mussum", "Carl Johnson", "Seu Madroga", "Amostradinho maldito", "cristiano da silva sauro pinto alcantra caicique classico jorge do pneu broxa azulzinho vencido da rosa vulgo tadala junior"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
